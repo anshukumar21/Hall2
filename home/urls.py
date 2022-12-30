8882524eb81c0feb665a3e10f1619e1fd8ccf62a
Vijaya Anand <sunrockers8@gmail.com>
2022-12-31 04:20:39 +0530

    Made query submission form

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home_view,name='home'),
-    path('gallery',views.gallery_view,name='gallery'),
+    path('home/',views.home_view,name='home'),
+    path('gallery/',views.gallery_view,name='gallery'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from rest_framework.decorators import api_view
+from .serializers import QuerySerializer
+from rest_framework.response import Response
 # Create your views here.
 
+@api_view(['GET','POST'])
 def home_view(request):
+    if request.method == "POST":
+        data_dict = {
+        "name" : request.POST["name"], 
+        "email" : request.POST["email"], 
+        "query" : request.POST["query"],}
+        serializer = QuerySerializer(data = data_dict)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response("Error")
     return render(request,"index.html")
 
 def gallery_view(request):
