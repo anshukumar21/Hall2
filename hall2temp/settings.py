bccd88b8069ac8a675db92467cde27762d120616
Anshu kumar <anshukr21@iitk.ac.in>
2023-01-05 15:38:53 +0530

    profile page style changes

--- hall2temp/settings.py
+++ hall2temp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'query',
     'rest_framework',
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static/')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
